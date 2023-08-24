b587a52827cd53e5476327e1ed0c4460878ae32c
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-24 13:37:11 +0300

    image and more validation

--- middleware.js
+++ middleware.js
@@ -7,10 +7,11 @@ function errorDetails(errorsArr) {
   }));
 }
 const postSchema = Joi.object({
-  title: Joi.string().required(),
-  body: Joi.string().required(),
-  author: Joi.string().required(),
-  tags: Joi.array().items(Joi.string()),
+  title: Joi.string().min(3).max(255).required(),
+  image: Joi.string().min(5).max(255).required(),
+  body: Joi.string().min(10).required(),
+  author: Joi.string().min(3).required(),
+  tags: Joi.array().items(Joi.string()).required(),
   date: Joi.date().iso().required(),
 }).options({ abortEarly: false });
 
@@ -23,7 +24,9 @@ const postSchema = Joi.object({
 const validatePost = async (req, res, next) => {
   console.log('req.body ===', req.body);
   try {
-    const validationResult = await postSchema.validateAsync(req.body, { abortEarly: false });
+    const validationResult = await postSchema.validateAsync(req.body, {
+      abortEarly: false,
+    });
     console.log('validationResult ===', validationResult);
     next();
   } catch (error) {
