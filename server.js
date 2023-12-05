4f2d3377d81cc247e45be75b197741f8e7e04ce3
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-05 11:58:52 +0200

    imge field

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  console.log('Serveris paleistas');
   console.log(`Server is running on port ${PORT}`);
 });
 
